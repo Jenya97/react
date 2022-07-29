6413da1ed1ba88d9c716258226da1f8d869b1b5c
Jenya97 <grigoryan_jenya@bk.ru>
2022-07-29 21:25:53 +0400

    react state

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Example from './Example';
+
+function App() {
+  return (
+    <div>
+    <Example name="example1" age="12" src="one.jpg" />
+    <Example name="example2" age="13" src="two.jpg"/>
+    <Example name="example3" age="14" src="three.jpg"/>
+    <Example name="example4" age="15" src="four.jpg"/>
+    </div>
+  );
+}
+
+export default App;
